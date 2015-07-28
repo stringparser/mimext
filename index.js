e6560269c08597af1fc9494f36200c8492bef1bb
Javier Carrillo <stringparser@gmail.com>
2015-07-28 19:33:09 +0200

    dev: be a little bit more flexible

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 'use strict';
 
+var path = require('path');
+
 exports = module.exports = mimext;
 
 mimext.mimes = require('mime-db');
 mimext.extnames = require('./extnames.json');
 
-function mimext(str){
-  if(typeof str !== 'string'){
-    throw new TypeError('1st argument should be a string');
-  } else if(str[0] === '.'){
-    return exports.extnames[str.slice(1)];
+function mimext(obj){
+  if(!obj){ return ; }
+
+  if(/\//.test(obj)){
+    return exports.mimes[obj];
+  } if(/\./.test(obj)){
+    return exports.extnames[path.extname(obj).slice(1) || obj.slice(1)];
+  } else if(obj && obj.extensions){
+    return exports.extnames[obj.extensions[0]];
   }
 
-  return exports.extnames[str] || exports.mimes[str] || null;
+  return exports.extnames[obj];
 }
